4d59c24624f7a1c73531d4d46f598affc2c3b91f
박성우 <stevepark252@gmail.com>
2023-05-26 14:45:03 +0000

    Added clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,2 +1,15 @@
 const clock = document.querySelector("h2#clock");
 
+function getClock(){
+    const date=new Date();
+    
+    //padStart(number,char)는 string이 number보다 작을 때 '앞'을 char로 채움
+    const hours=String(date.getHours()).padStart(2,0);
+    const minutes=String(date.getMinutes()).padStart(2,0);
+    const seconds=String(date.getSeconds()).padStart(2,0);
+
+    clock.innerText=(`${hours}:${minutes}:${seconds}`);
+}
+
+getClock();
+setInterval(getClock, 1000);
---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+const quotes=[
+    {
+        quote:"The mark of the immature man is that he wants to die nobly for a cause, while the mark of the mature man is that he wants to live humbly for one.",
+        author:"Wilhelm Stekel",
+    },
+    {
+        quote:"I'd rather be optimistic and wrong than pessimistic and right.",
+        author:"Elon Musk",
+    },
+    {
+        quote:"ZERO TO ONE EVERY MOMENT IN BUSINESS happens only once. The next Bill Gates will not build an operating system. The next Larry Page or Sergey Brin won’t make a search engine. And the next Mark Zuckerberg won’t create a social network. If you are copying these guys, you aren’t learning from them.",
+        author:"Peter Thiel",
+    },
+    {
+        quote:"Once you’re 10x better, you escape competition.",
+        author:"Peter Thiel",
+    },
+    {
+        quote:"The more we regard our success as our own doing, the less responsibility we feel for those who fall behind.",
+        author:"Michael Sandel",
+    },
+    {
+        quote:"In an unequal society, those who land on top want to believe their success is morally justified. In a meritocratic society, this means the winners must believe they have earned their success through their own talent and hard work.",
+        author:"Michael Sandel",
+    },
+    {
+        quote:"Only nuclear, not solar and wind, can provide abundant, reliable, and inexpensive heat.",
+        author:"Michael Shellenberger",
+    },
+    {
+        quote:"The artificial things are as natural as the natural things.",
+        author:"Michael Shellenberger",
+    },
+    {
+        quote:"",
+        author:"",
+    },
+    {
+        quote:"",
+        author:"",
+    },
+]
