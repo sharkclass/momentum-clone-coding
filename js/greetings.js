bfab741865cbf21109633bad152c35d91cb1256d
박성우 <stevepark252@gmail.com>
2023-05-26 13:43:29 +0000

    Completed login-form v2 and organized folder

--- js/greetings.js
+++ js/greetings.js
@@ -9,7 +9,7 @@ function onLoginSubmit(event){
     event.preventDefault(); //preventDefault stops default behavior of browser and javascript gives event value to first argument of fuction for free when addEventListener activates.
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username=loginInput.value;
-    localStorage.setItem("USERNAME_KEY",username); //browser에 값을 저장해 새로고침해도 값을 기억하도록 함. (key,value)형식으로 작성
+    localStorage.setItem(USERNAME_KEY,username); //browser에 값을 저장해 새로고침해도 값을 기억하도록 함. (key,value)형식으로 작성
     paintGreetings(username);
 }
 
@@ -18,7 +18,7 @@ function paintGreetings(username){
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername=localStorage.getItem("USERNAME_KEY");
+const savedUsername=localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername==null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);//show the form
